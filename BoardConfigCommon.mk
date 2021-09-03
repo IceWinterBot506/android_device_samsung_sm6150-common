90c4c7a57d793e8cb84d67e3eefbeb744bc78a48
Haky86 <hakamelassouad@gmail.com>
2021-09-03 21:00:25 +0200

    sm6150-common: Setup arch flags
    Change-Id: I98624b3b93e3464061fc251f2c9002405db8ae56
    Signed-off-by: IceWinterBot506 <mydadisbrave3@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,5 +16,26 @@
 
 COMMON_PATH := device/samsung/sm6150-common
 
+# 64 Bit
+TARGET_USES_64_BIT_BINDER := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
+# Audio
+USE_XML_AUDIO_POLICY_CONF := 1
+
 # Inherit the proprietary files
 include vendor/samsung/sm6150-common/BoardConfigVendor.mk
