d72f68555e3b010a96d38687ae24ef2aa6ea7125
Rahul Krishna <Rahulkrishna585@gmail.com>
2021-09-05 01:20:44 +0200

    sm6150-common: Import WiFi flags and build wifi packages
    Change-Id: I3d03824600f7d9ddab4c76de2b027133fc7f3c09
    Signed-off-by: IceWinterBot506 <mydadisbrave3@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -92,6 +92,14 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
 
 # Wifi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    libwifi-hal-qcom \
+    wcnss_service \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(COMMON_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
