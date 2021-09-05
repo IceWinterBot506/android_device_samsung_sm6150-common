850c64a5b4895d5ccd7576f2fc1a79b7ef028284
codeworkx <daniel.hillenbrand@codeworkx.de>
2021-09-05 02:28:43 +0200

    sm6150-common: build libbthost_if from hardware/qcom
    Change-Id: I02b80be3f9d06bccd85fe568c600f7557aca8883
    Signed-off-by: IceWinterBot506 <mydadisbrave3@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -89,6 +89,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbthost_if
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
